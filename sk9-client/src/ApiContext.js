0c0117816f338ecfb593d855c973a4746a4917de
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-02-23 00:39:45 -0700

    added most directories and working on app js basics

--- sk9-client/src/ApiContext.js
+++ sk9-client/src/ApiContext.js
@@ -1,5 +1,11 @@
-export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
-    API_KEY: process.env.REACT_APP_API_KEY,
-  }
+import React from 'react'
+
+export default React.createContext({
+  sessions: [],
+  folders: [],
+  addFolder: () => {},
+  addSession: () => {},
+  deleteSession: () => {},
+})
+
   
--- sk9-client/src/App/App.js
+++ sk9-client/src/App/App.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { Route, Link } from "react-router-dom"
+import config from "../config"
+import "./App.css"
+import ApiContext from "../ApiContext"
+import AddFolder from "../AddFolder/AddFolder"
+import AddSession from "../AddSession/AddSession"
+import MainNav from "../MainNav/MainNav"
+import UserHome from "../UserHome/UserHome"
+import SessionDetail from "../SessionDetail/SessionDetail"
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+
+class App extends Component {
+  state = {
+    sessions: [],
+    folders: [],
+  };
+
+
+
+  renderMainRoutes() {
+    return (
+      <>
+        {["/", "/folder/:folderId"].map((path) => (
+          <Route exact key={path} path={path} component={UserHome} />
+        ))}
+        <Route path="/session/:sessionId" component={SessionDetail} />
+        <Route path="/add-folder" component={AddFolder} />
+        <Route path="/add-session" component={AddSession} />
+      </>
+    );
+  }
+
+  render() {
+    const value = {
+      sessions: this.state.sessions,
+      folders: this.state.folders,
+      addFolder: this.handleAddFolder,
+      addSession: this.handleAddSession,
+      deleteSession: this.handleDeleteSession,
+    };
+    return (
+    <ApiContext.Provider value={value}>
+      <div className="App">
+        <nav className="App__nav">{this.renderNavRoutes()}</nav>
+        <header className="App__header">
+          <h1>
+            <Link to="/">SK9</Link>{" "}
+          </h1>
+        </header>
+        <main className="App__main">{this.renderMainRoutes()}</main>
+      </div>
+    </ApiContext.Provider>
+    )
+  }
 }
 
 export default App;
